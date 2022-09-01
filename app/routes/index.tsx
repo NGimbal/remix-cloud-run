7ef873465f2608f3862e8b2363cb6271b1c51e73
NGimbal <nicolas.p.schmidt@gmail.com>
2022-09-01 17:32:07 -0400

    change some shit to see what happens

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Welcome to Remix in a Docker Container</h1>
       <ul>
         <li>
           <a
